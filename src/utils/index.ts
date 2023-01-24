fe47c68766b514e86e972c3bf24571d4953234b1
Igor <igorgordilov22@gmail.com>
2023-01-24 17:25:42 +0300

    1

---
+++ src/utils/index.ts
@@ -0,0 +1,7 @@
+
+
+export const ETHSCAN_API = `https://api.nft.fluuu.id/prod/ethusd`
+export const METADATA_API = 'https://api.nft.fluuu.id/prod'
+
+export * from './toShort'
+export * from './formatPriceEth'
